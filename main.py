b3b8aab61b08e6dc23e7374b398b6acf67ab22b2
Elinor <46898565+elthorne@users.noreply.github.com>
2025-12-04 17:13:59 +0000

    wip: TODO
    refs: DSIT-192

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def webhook():
     logging.info("Incoming webhook")
 
     payload = request.json
+
+    # TODO: if payload["action"] == "created": go to a specific handler, etc
     teams_card = build_teams_dependabot_card(payload)
     return send_to_teams(teams_card)
 
