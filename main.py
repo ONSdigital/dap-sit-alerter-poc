0e421f5fe8c693ad8be9906cdb16e262a84a40f4
Elinor <46898565+elthorne@users.noreply.github.com>
2025-12-11 12:20:00 +0000

    wip: placeholders and TODOs
    refs: DSIT-192

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import logging
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
-from src.handlers.incoming_request import build_teams_dependabot_card
-from src.handlers.outgoing_request import send_to_teams
+from src.helpers import load_sla_config
+from src.models.dependabot_alert_model import DependabotAlert
+from src.services.outgoing import send_to_teams
+from src.services.teams_card_builder import TeamsCardBuilder
 
 app = Flask(__name__)
 
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({'status': 'healthy'}), 200
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    logging.info("Incoming webhook")
-
+    logging.info("Incoming GitHub webhook")
     payload = request.json
 
-    # TODO: if payload["action"] == "created": go to a specific handler, etc
-    teams_card = build_teams_dependabot_card(payload)
+    # TODO: Refactor - handler pattern
+    alert = DependabotAlert.from_webhook(payload)
+
+    # TODO: Configure output channel, i.e., Teams, Slack, email, fax, etc
+    sla_config = load_sla_config()
+    teams_card_builder = TeamsCardBuilder(sla_config)
+    teams_card = teams_card_builder.build_card(alert)
+
     return send_to_teams(teams_card)
 
 
---
+++ src/services/outgoing.py
@@ -0,0 +1,8 @@
+def send_to_teams(payload):
+    return "ok"
+
+def send_to_slack(payload):
+    return "ok"
+
+def send_to_email(payload):
+    return "ok"
