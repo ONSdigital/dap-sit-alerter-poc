75c40f3300e1b09f8d42c5c5c416b2a0ee8eea5f
Elinor <46898565+elthorne@users.noreply.github.com>
2025-12-11 12:43:08 +0000

    docs: added TODO to question desired behaviour
    refs: DSIT-192

--- src/models/dependabot_alert_model.py
+++ src/models/dependabot_alert_model.py
@@ -13,6 +13,7 @@ class DependabotAlert:
 
     @classmethod
     def from_webhook(cls, payload: Dict[str, Any]):
+        # TODO: Is defaulting to None the desired behaviour when it can't be found?
         alert = payload.get("alert", {})
         dependency = alert.get("dependency", {})
         package = dependency.get("package", {})
