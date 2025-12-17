d0829f0f163a65d15c64b84abc48fb7c080664f8
Elinor <46898565+elthorne@users.noreply.github.com>
2025-12-17 10:02:42 +0000

    refactor: extract orchestration layer from endpoints.py - 1st draft!
    refs: DSIT-192

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,13 +1,9 @@
 import logging
-import os
 
-from flask import Blueprint, jsonify, request, abort
+from flask import Blueprint, jsonify, request
 
-from app.auth import verify_github_signature, verify_github_event
-from src.helpers import load_sla_config
-from src.models.dependabot_alert_model import DependabotAlert
-from src.services.outgoing import send_to_teams
-from src.services.teams_card_builder import TeamsCardBuilder
+from app.handlers.dependabot_handler import DependabotHandler
+
 
 incoming = Blueprint("endpoints", __name__)
 
@@ -30,24 +26,4 @@ def health():
 @incoming.route('/webhook', methods=['POST'])
 def webhook():
     logging.info("Incoming GitHub webhook")
-
-    secret = os.environ.get('GITHUB_WEBHOOK_SECRET')
-    if not verify_github_signature(secret):
-        abort(401, description="Unauthorised")
-
-    if not verify_github_event():
-        abort(401, description="Unauthorised")
-
-    payload = request.json
-
-    # TODO: Refactor - handler pattern
-    alert = DependabotAlert.from_webhook(payload)
-
-    # TODO: Defensive programming.  Values cannot be 0/null/None, etc, and test it
-    sla_config = load_sla_config()
-
-    # TODO: Configure output channel, i.e., Teams, Slack, email, fax, etc
-    teams_card_builder = TeamsCardBuilder(sla_config)
-    teams_card = teams_card_builder.build_card(alert)
-
-    return send_to_teams(teams_card)
+    return DependabotHandler().handle_webhook(request)
---
+++ app/handlers/dependabot_handler.py
@@ -0,0 +1,48 @@
+import os
+
+from flask import abort
+
+from app.auth import verify_github_signature, verify_github_event
+from src.helpers import load_sla_config
+from src.models.dependabot_alert_model import DependabotAlert
+from src.services.outgoing import send_to_teams
+from src.services.teams_card_builder import TeamsCardBuilder
+
+class Response:
+    def __init__(self, status: int, body: str):
+        self.status = status
+        self.body = body
+
+
+class DependabotHandler:
+    def handle_webhook(self, payload) -> Response:
+        # security
+        secret = os.environ.get('GITHUB_WEBHOOK_SECRET')
+        if not verify_github_signature(secret):
+            abort(401, description="Unauthorised")
+
+        if not verify_github_event():
+            abort(401, description="Unauthorised")
+
+        payload = payload.json
+
+        # validate action, i.e., created, reopened, fixed, etc
+
+        # load config
+        # TODO: Defensive programming.  Values cannot be 0/null/None, etc, and test it
+        sla_config = load_sla_config()
+
+        # parse alert
+        alert = DependabotAlert.from_webhook(payload)
+
+        # build Teams card
+        teams_card_builder = TeamsCardBuilder(sla_config)
+        teams_card = teams_card_builder.build_card(alert)
+
+        # send to Teams
+        success = send_to_teams(teams_card)
+
+        if not success:
+            return Response(400, f"Failed to send alert to Teams")
+
+        return Response(200, f"Alert {alert.alert_number} sent to Teams")
