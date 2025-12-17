37b9c6380ac5871cd1e8d095ca16b73ba871ae36
Elinor <46898565+elthorne@users.noreply.github.com>
2025-12-17 13:08:34 +0000

    docs: add TODO
    refs: DSIT-192

--- src/helpers.py
+++ src/helpers.py
@@ -4,6 +4,7 @@ import yaml
 from pathlib import Path
 
 
+# TODO: Move this to the SLO Config model
 def load_slo_config(path: str = None) -> Dict[str, Any]:
     if path is None:
         project_root = Path(__file__).resolve().parents[1]
