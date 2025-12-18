4c7e4fa922b201641b0377c532c9d56f5a0187b3
Elinor <46898565+elthorne@users.noreply.github.com>
2025-12-18 15:33:11 +0000

    docs: add docstring to function explaining why it's been stubbed
    refs: DSIT-192

--- src/teams/teams_notifier.py
+++ src/teams/teams_notifier.py
@@ -3,12 +3,10 @@ from typing import Dict, Any
 
 # TODO: Create custom Teams Card type
 def send_to_teams(card: Dict[str, Any], teams_connector_url: str):
+    """
+    This function has been stubbed until integration details with Teams has been authorised and configured
+    """
     print("Posting connector card to Teams...")
     import json
     print(json.dumps(card, indent=4))
     return True
-
-    # headers = {'Content-Type': 'application/json'}
-    # response = requests.post(
-    #     teams_connector_url, json=card, headers=headers)
-    # return response.status_code
