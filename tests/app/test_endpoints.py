d4a2963c987ec5754f594419a2c65cf1f1519126
Elinor <46898565+elthorne@users.noreply.github.com>
2025-12-18 15:24:40 +0000

    test: trivial tests added to test endpoints
    refs: DSIT-192

--- tests/app/test_endpoints.py
+++ tests/app/test_endpoints.py
@@ -1,11 +1,34 @@
 import json
+from unittest.mock import patch, MagicMock
 
+import pytest
 from dotenv import load_dotenv
+from flask import Response, Flask
+
+from app.endpoints import add_header, incoming
 
 load_dotenv()
 
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    app.config['TEAMS_CONNECTOR_URL'] = "http://dummy-url"
+    app.register_blueprint(incoming)
+    return app
 
-def assert_security_headers_are_present(response):
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_add_header_configures_the_response_headers_per_security_guidance():
+    # arrange
+    response = Response()
+
+    # act
+    response = add_header(response)
+
+    # assert
     assert response.headers["X-Content-Type-Options"] == "nosniff"
     assert response.headers["Strict-Transport-Security"] == "max-age=86400; includeSubDomains"
     assert response.headers["Cache-Control"] == "no-store"
@@ -21,4 +44,26 @@ def test_health_check(client):
     # assert
     assert json.loads(response.get_data(as_text=True)) == {"healthy": True}
     assert response.status_code == 200
-    assert_security_headers_are_present(response)
+
+
+@patch("app.endpoints.DependabotHandler")
+def test_webhook_calls_handle_webhook_with_the_correct_parameters(
+        mock_handler,
+        incoming_github_dependabot_webhook,
+        client
+):
+    # arrange
+    mock_instance = MagicMock()
+    mock_instance.handle_webhook.return_value = Response("ok", 200)
+    mock_handler.return_value = mock_instance
+
+    # act
+    response = client.post("/webhook", json=incoming_github_dependabot_webhook)
+
+    # assert
+    mock_instance.handle_webhook.assert_called_once()
+
+    args, kwargs = mock_instance.handle_webhook.call_args
+    assert args[1] == "http://dummy-url"
+    assert response.status_code == 200
+    assert response.data == b"ok"
